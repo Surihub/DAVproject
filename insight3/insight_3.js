baf81ce17a2580f335a7b75988981a984f8aec1b
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2023-11-30 22:27:27 +0900

    Add files via upload

---
+++ insight3/insight_3.js
@@ -0,0 +1,121 @@
+var countriesData = [
+    { "label": "USA", "row": 9, "column": 2 },
+    { "label": "CAN", "row": 8, "column": 2 },
+    { "label": "MEX", "row": 10, "column": 2 },
+    { "label": "GTM", "row": 11, "column": 2 },
+    { "label": "KOR", "row": 8, "column": 26 },
+    { "label": "CHN", "row": 7, "column": 25 },
+    { "label": "JPN", "row": 7, "column": 28 },
+    { "label": "MNG", "row": 6, "column": 28 },
+    { "label": "FIN", "row": 2, "column": 18 },
+    { "label": "NZL", "row": 22, "column": 27 },
+
+    // ... 업데이트 해야 함(약 200개국)
+];
+
+
+// Set the dimensions of the grid
+var width = 1500,
+height = 1000,
+gridSize = 50;
+
+// 국가별 데이터로 버튼 생성하고 'gridContainer'안에 넣기
+countriesData.forEach(function(country) {
+    var btn = document.createElement("button");
+    btn.innerHTML = country.label;
+    btn.style.backgroundColor = country.color;
+    btn.style.gridRowStart = country.row;
+    btn.style.gridColumnStart = country.column;
+    btn.onclick = function() { plotCountryData(country.label); };
+    document.getElementById("gridContainer").appendChild(btn);
+});
+console.log(calculateColor(2020));
+function calculateColor(year) {
+    // 예시: year 값이 2000에서 2020 사이라고 가정
+    var maxYear = 2020; // 가장 최근 연도
+    var minYear = 2000; // 가장 오래된 연도
+
+    // 연도에 따라 0에서 255 사이의 값을 계산
+    var intensity = 255 * (1 - (year - minYear) / (maxYear - minYear));
+
+    // RGB 형식으로 색상 코드 반환
+    return "rgb(" + intensity + "," + intensity + "," + intensity + ")";
+}
+
+
+
+
+
+// Load the data, 전체 국가 산점도 그리기
+d3.csv('backdated SDG Index.csv').then(function(data) {
+// d3.csv('SDR 2023 Data.csv').then(function(data) {
+    // Populate dropdown menus
+    populateDropdowns();
+
+    // Initial plot
+    updateScatterPlot(data, 'population', 'SDG Index Score');  // Default indices for initial plot
+
+    // Event listeners for dropdown changes
+    d3.select('#index1').on('change', function() {
+        updatePlot(data);
+    });
+    d3.select('#index2').on('change', function() {
+        updatePlot(data);
+    });
+});
+
+// year = 2022...
+function populateDropdowns() {
+    const options = ['population','SDG Index Score','Goal 1 Score','Goal 2 Score','Goal 3 Score','Goal 4 Score','Goal 5 Score','Goal 6 Score','Goal 7 Score','Goal 8 Score','Goal 12 Score','Goal 13 Score'];
+    options.forEach(function(option) {
+
+        d3.select('#index1').append('option').text(option).attr('value', option);
+        d3.select('#index2').append('option').text(option).attr('value', option);
+    });
+}
+
+function plotCountryData(countryCode) {
+    d3.csv('backdated SDG Index.csv').then(function(data) {
+        // Filter data for the selected country
+        var countryData = data.filter(function(d) {
+            return d['Country Code ISO3'] === countryCode; // Adjust the property name as per your CSV
+        });
+        var index1 = d3.select('#index1').node().value;
+        var index2 = d3.select('#index2').node().value;
+        updateScatterPlot(countryData, index1, index2);
+        // updateScatterPlot(countryData, 'population', 'SDG Index Score');
+        // index1, 2 input으로 받도록 추가
+    });
+}
+
+function updatePlot(data) {
+    var index1 = d3.select('#index1').node().value;
+    var index2 = d3.select('#index2').node().value;
+    updateScatterPlot(data, index1, index2);
+}
+
+// 마우스 오버 이벤트 처리 함수
+// function mouseover(event, d) {
+//     // 현재 마우스 오버된 점의 크기와 색상 변경
+//     d3.select(this)
+//       .transition()
+//       .duration(150)
+//       .attr('r', 5)
+//       .attr('fill', '#ff5722');  // 색상을 변경 (예: 빨간색)
+  
+//     // 다른 모든 점을 회색으로 변경
+//     circles.filter((_, i, nodes) => nodes[i] !== this)
+//       .transition()
+//       .duration(150)
+//       .attr('fill', '#ccc');
+  
+//     // 툴팁 표시
+//     tooltip
+//       .style('opacity', '0.8')
+//       .html(ind_x+':'+ d[ind_x]
+//       + '<br/>' + ind_y + ':' + d[ind_y]
+//       + '<br/>' + "country" + ':' + d.country)
+//       .style('left', (event.x + 10) + 'px')
+//       .style('top', (event.y - 80) + 'px');
+//   }
+  
---
+++ insight3/map.js
@@ -0,0 +1,88 @@
+function plotCountryData(countryCode) {
+    d3.csv('backdated SDG Index.csv').then(function(data) {
+        // Filter data for the selected country
+        console.log('성공!!');
+        var countryData = data.filter(function(d) {
+            return d['Country Code ISO3'] === countryCode; // Adjust the property name as per your CSV
+        });
+        updateScatterPlot(countryData, 'population', 'SDG Index Score');
+        // index1, 2 input으로 받도록 추가
+    });
+}
+
+function updateScatterPlot(data, index1, index2) {
+    // Clear existing SVG content
+    d3.select("svg").selectAll("*").remove();
+
+    // Set up SVG dimensions
+    var svgWidth = 800, svgHeight = 600;
+    var margin = { top: 20, right: 20, bottom: 60, left: 60 };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+
+    // Create scaling functions
+    var x = d3.scaleLinear()
+        .domain([d3.min(data, d => +d[index1]), d3.max(data, d => +d[index1])])
+        .range([0, width]);
+
+    var y = d3.scaleLinear()
+        .domain([d3.min(data, d => +d[index2]), d3.max(data, d => +d[index2])])
+        .range([height, 0]);
+
+    // Append 'g' element to the SVG
+    var g = d3.select("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // X and Y axes
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .call(d3.axisLeft(y));
+
+    // Bind data to circles
+    var circles = g.selectAll(".dot")
+        .data(data, function(d) { return d.id || d[index1] + d[index2]; });
+
+    // Enter + update selection
+    circles.enter().append("circle")
+        .attr("class", "dot")
+        .attr("cx", svgWidth / 2) // Start new circles from the center
+        .attr("cy", svgHeight / 2)
+        .attr("r", 0) // Start with radius 0
+        .merge(circles) // Merge enter and update selections
+        .transition() // Apply transition
+        .duration(500)
+        .attr("cx", d => x(d[index1]))
+        .attr("cy", d => y(d[index2]))
+        .attr("r", 5)
+        .attr("fill", d => calculateColor(d.year));
+
+    // Exit selection
+    circles.exit()
+        .transition()
+        .duration(500)
+        .attr("cx", svgWidth / 2) // End at the center
+        .attr("cy", svgHeight / 2)
+        .attr("r", 0) // Shrink to radius 0
+        .remove();
+    // Axis labels
+    g.append("text")
+        .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text(index1);
+
+    g.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text(index2);
+}
+
+
